8ce90f023172f110b9d17a7d6b3c2fb2859f5451
Bartek Jedlinski <b.jedlinski@bewpage.com>
2023-04-20 19:59:55 +0200

    feat: basic component configuration

--- src/app/layout/recipes/recipe-list/recipe-list.component.ts
+++ src/app/layout/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {}
+export class RecipeListComponent {
+  recipeList: Recipe[] = [
+    new Recipe(
+      'Chicken Breast',
+      'Chicken breasts are the most popular food in the world.',
+      'https://placehold.jp/3d4070/ffffff/150x150.png?text=recipe_01'
+    ),
+    new Recipe(
+      'Creamy Chicken Pasta',
+      'Creamy chicken pasta with mushrooms in a creamy sauce.',
+      'https://placehold.jp/3d4070/ffffff/150x150.png?text=recipe_02'
+    ),
+    new Recipe(
+      'Chicken Parmesan',
+      'Chicken Parmesan is a classic Italian-American dish.',
+      'https://placehold.jp/3d4070/ffffff/150x150.png?text=recipe_03'
+    ),
+  ];
+}
--- src/app/layout/shopping-list/shopping-list.component.ts
+++ src/app/layout/shopping-list/shopping-list.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent {}
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+    new Ingredient('Cucumbers', 15),
+    new Ingredient('Strawberries', 20),
+    new Ingredient('Onions', 25),
+    new Ingredient('Mushrooms', 30),
+    new Ingredient('Pears', 35),
+  ];
+}
