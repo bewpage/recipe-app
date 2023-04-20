e929789206bb80b94282eb7d9ba647ef0a577921
Bartek Jedlinski <b.jedlinski@bewpage.com>
2023-04-20 14:42:33 +0200

    feat: component structure setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { RecipesComponent } from './layout/recipes/recipes.component';
+import { RecipeListComponent } from './layout/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './layout/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './layout/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './layout/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './layout/shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
