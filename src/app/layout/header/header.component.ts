c5d655ba6764949ceae3b7c0290a4dc3ecbdc9db
Bartek Jedlinski <b.jedlinski@bewpage.com>
2023-04-21 17:43:34 +0200

    feat: add some navigation

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+
+  @Output() emitNavButtonClick = new EventEmitter<{ buttonName: string }>();
   collapsed = true;
+  buttonName: string;
+
+  onNavButtonClick = (event) => {
+    console.log('onNavButtonClick', event.target.textContent);
+    this.buttonName = event.target.textContent;
+    this.emitNavButtonClick.emit({ buttonName: event.target.textContent });
+  };
 }
